475dec180bec9c2113a78d3d9e422e8ac0d891b0
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-10 10:32:46 -0700

    Add home route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
 
 # Create your views here.
+# Define home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
