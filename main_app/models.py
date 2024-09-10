2d3a4548da54d4b9634d8431d176f3d246ec19ac
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-10 15:47:37 -0700

    Install postgresql to django env and initialize

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    
+# Good practice: override the __str__ method in a model so they print in a more helpful way
+    def __str__(self):
+        return self.name
