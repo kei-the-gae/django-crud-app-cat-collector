91b9cb38483eb7237985963969c69ffe3ca244dd
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-10 16:37:47 -0700

    Set up fetching data with orm and set up admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import Cat model
+from .models import Cat
 
 # Register your models here.
+admin.site.register(Cat)
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
+# Import Cat model
+from .models import Cat
 
 #Temporary import of cat data
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
 # Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 # Create your views here.
 # Define home view function
@@ -31,4 +33,5 @@ def about(req):
 
 # Define cat index view function
 def cat_index(req):
+    cats = Cat.objects.all() # look familiar?
     return render(req, 'cats/index.html', {'cats': cats})
