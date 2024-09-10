10595a1c90f839a186e67458f46717af034b5b73
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-10 10:45:11 -0700

    Add about route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
