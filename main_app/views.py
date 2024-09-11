45f3b614bdc459454e4a7efbfddcae4b8f38248d
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-11 03:16:01 -0700

    Add feeding form and conditionally render past feedings section

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
 # Import CreateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Import Cat model
 from .models import Cat
+from .forms import FeedingForm
 
 #Temporary import of cat data
 # class Cat:
@@ -40,8 +41,28 @@ def cat_index(req):
 
 # Define cat detail view function
 def cat_detail(req, cat_id):
+    # Instantiate cat to be rendered in the template
     cat = Cat.objects.get(id=cat_id)
-    return render(req, 'cats/detail.html', {'cat': cat})
+    # Instantiate FeedingForm
+    feeding_form = FeedingForm()
+    return render(req, 'cats/detail.html', {
+        # Include the cat and feeding_form in the context
+        'cat': cat,
+        'feeding_form': feeding_form
+    })
+
+# Define add feeding view function
+def add_feeding(req, cat_id):
+    # create a ModelForm instance using the data in req.POST
+    form = FeedingForm(req.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
+
 
 # Define create class-based views (CBV) class
 class CatCreate(CreateView):
@@ -55,7 +76,7 @@ class CatUpdate(UpdateView):
     model = Cat
     # Let's disallow the renaming of a cat by excluding the name field!
     fields = ['breed', 'description', 'age']
-    
+
 #Define delete CBV class
 class CatDelete(DeleteView):
     model = Cat
